c0e4d3e012fdab884625df7f87bab1ff0a579d47
nickmancini1 <nickmancini1@gmail.com>
2026-04-19 18:11:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9243,7 +9243,8 @@ async def safe_fast_on_demand_default_simple() -> Any:
         "simple_output": payload.get("simple_output"),
         "screened_best_context": payload.get("screened_best_context"),
         "failed_reasons": payload.get("failed_reasons"),
-    }
+
+        
 
 
 @app.post(
